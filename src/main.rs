91dd11ccf4543b103463dfce5cd86f1f0ab66044
thangchung <thangchung@ymail.com>
2020-05-10 20:00:10 +0700

    add logs

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,13 @@ async fn index(req: HttpRequest) -> Result<HttpResponse> {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    std::env::set_var("RUST_LOG", "actix_web=debug");
+
+    HttpServer::new(|| { 
+        App::new()
+            .service(index)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
