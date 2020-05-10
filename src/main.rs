382a166691af4e44f92429302a69a85c9b551749
thangchung <thangchung@ymail.com>
2020-05-10 19:49:20 +0700

    init repo

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use actix_web::http::{StatusCode};
+use actix_web::{get, App, HttpServer, HttpRequest, HttpResponse, Result};
+
+#[get("/")]
+async fn index(req: HttpRequest) -> Result<HttpResponse> {
+    println!("{:?}", req);
+
+    Ok(HttpResponse::build(StatusCode::OK)
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../static/welcome.html")))
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
